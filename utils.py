6a74605a2edfabe47c18487da6f98beb0afddde7
snewman-aa <snewman.aa@gmail.com>
2023-06-23 15:14:45 -0400

    set path root to /llc_data/clams

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import pathlib
 
-def media_path_dict(root="/data/clams/wgbh"):
+def media_path_dict(root="/llc_data/clams"):
     all_paths = pathlib.Path(root).glob("**/*.mp4")
-    return {p.name : str(p.absolute()) for p in all_paths}
+    return {p.stem.split('.')[0]: str(p.absolute()) for p in all_paths}
 
-def add_label_column(csv_filepath: str):
-    # load a csv file and add a column label with the value "chyron"
+def add_label_column(csv_filepath: str, label_column: str='label', default_label: str='chyron'):
+    # load a csv file and check if the label column exists, if not, add it with the default label
     import pandas as pd
     df = pd.read_csv(csv_filepath)
-    df["label"] = "chyron"
+    if label_column not in df.columns:
+        df[label_column] = default_label
     df.to_csv(csv_filepath, index=False)
