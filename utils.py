4f4083652a3206fdacfc3db59adad6f9c9e99bd4
Keigh Rim <keigh.rim@gmail.com>
2023-10-02 16:22:37 -0400

    refactored build_index script, added a quick annotation on credits on a subset of `aapb-annotation-44` batch
    * see https://raw.githubusercontent.com/clamsproject/aapb-annotations/main/batches/aapb-annotation-44.txt and https://github.com/clamsproject/aapb-annotations/issues/44 .

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 import pathlib
+from urllib.request import urlopen
+from urllib.parse import quote
+import os 
+import json
+import sys
 
 def media_path_dict(root="/data/clams/"):
     all_paths = list(pathlib.Path(root).glob("**/*.mp4"))
@@ -13,3 +18,10 @@ def add_label_column(csv_filepath: str, label_column: str='label', default_label
     if label_column not in df.columns:
         df[label_column] = default_label
     df.to_csv(csv_filepath, index=False)
+
+def video_path_baapb(guid):
+    RESOLVER_ADDRESS = os.environ['BAAPB_RESOLVER_ADDRESS']
+    url = 'http://' + RESOLVER_ADDRESS + '/searchapi'
+    qstr = "&".join(map(lambda x: f"{x[0]}={quote(x[1])}", {'file': 'video', 'guid': guid}.items()))
+    res = urlopen("?".join((url,qstr)))
+    return json.loads(res.read().decode('utf-8'))[0]
